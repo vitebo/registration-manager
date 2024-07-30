bd87bafe4c62f39efb0ffe5ef546be16c40f448e
vitebo <andre.vitebo@quero.com>
2024-07-30 16:07:55 -0300

    chore(contex): set default value for di

--- src/contexts/di-context.ts
+++ src/contexts/di-context.ts
@@ -1,8 +1,11 @@
 import { createContext } from "react";
 import {RegistrationGateway} from "~/gateways/registration-gateway.ts";
+import { RegistrationGatewayMemory } from '~/gateways/registration-gateway-memory'
 
 interface DiContextValue {
     registrationGateway: RegistrationGateway;
 }
 
-export const DiContext = createContext<DiContextValue | undefined>(undefined);
+export const DiContext = createContext<DiContextValue>({
+    registrationGateway: new RegistrationGatewayMemory(),
+});
