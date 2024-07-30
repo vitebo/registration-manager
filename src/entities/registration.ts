0e51db58e3580e9ef0ce6c20f18e5aafdf837066
vitebo <andre.vitebo@quero.com>
2024-07-30 10:39:34 -0300

    chore(registration): use the other entities

--- src/entities/registration.ts
+++ src/entities/registration.ts
@@ -1,16 +1,29 @@
 import RegistrationStatus, { RegistrationStatusFactory, RegistrationStatusEnum } from "./registration-status"
+import  { EmployeeName } from './employee-name'
+import { Email } from './email'
+import { Cpf } from './cpf'
 
 export class Registration {
+    public readonly id
+    public employeeName: EmployeeName;
+    public cpf: Cpf;
+    public email: Email;
+    public admissionDate: string;
     public status: RegistrationStatus;
 
     constructor(
-        public readonly id: string,
-        public readonly employeeName: string,
-        public readonly cpf: string,
-        public readonly email: string,
-        public readonly admissionDate: string,
+        id: string,
+        employeeName: string,
+        cpf: string,
+        email: string,
+        admissionDate: string,
         status: keyof typeof RegistrationStatusEnum
     ) {
+        this.id = id;
+        this.employeeName = new EmployeeName(employeeName);
+        this.cpf = new Cpf(cpf);
+        this.email = new Email(email);
+        this.admissionDate = admissionDate;
         this.status = RegistrationStatusFactory.create(status, this);
     }
 }
