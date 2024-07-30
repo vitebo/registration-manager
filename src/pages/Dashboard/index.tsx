b9b87193ced39f4057286b9e0192f559d95899d5
vitebo <andre.vitebo@quero.com>
2024-07-30 18:03:10 -0300

    feat(dashboard): fetch registrations on load

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,21 @@
+import { useEffect, useContext } from 'react';
 import Collumns from './components/Columns';
 import * as S from './styles';
 import { SearchBar } from './components/Searchbar';
-import { useContext } from 'react';
 import { StoreContext } from '~/contexts/store-context.ts';
+import { DiContext } from '~/contexts/di-context';
 
 const DashboardPage = () => {
-  const { registrations } = useContext(StoreContext);
+  const { registrations, setRegistrations } = useContext(StoreContext);
+  const { registrationGateway } = useContext(DiContext);
+
+  useEffect(() => {
+    const fetchRegistrations = async () => {
+      const registrations = await registrationGateway.get();
+      setRegistrations(registrations);
+    };
+    fetchRegistrations();
+  }, []);
 
   return (
     <S.Container>
