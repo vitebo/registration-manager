18b647b1a9e611a3d741535ee212133e67e33568
vitebo <andre.vitebo@quero.com>
2024-07-30 18:57:51 -0300

    chore(eslint): configure import rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,16 @@
 module.exports = {
   extends: [
     'eslint:recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
     'plugin:react/recommended',
     'plugin:import/recommended',
     'plugin:jsx-a11y/recommended',
     'plugin:@typescript-eslint/recommended',
-    'eslint-config-prettier',
-    'plugin:react-hooks/recommended'
+    'plugin:react-hooks/recommended',
+    'eslint-config-prettier'
   ],
+  plugins: ['@typescript-eslint', 'import'],
   settings: {
     react: {
       version: 'detect'
@@ -28,7 +31,28 @@ module.exports = {
     'react/display-name': 'off',
     '@typescript-eslint/ban-types': 'off',
     'import/named': 'off',
-    'react-hooks/exhaustive-deps': 'warn'
+    'react-hooks/exhaustive-deps': 'warn',
+    'import/no-internal-modules': [
+      'error',
+      { allow: ['~/*', '~/pages/*', '~/components/*', 'react-dom/client', 'react-icons/hi', '~~/**/*'] }
+    ],
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always',
+        groups: ['builtin', 'external', 'internal', 'index'],
+        warnOnUnassignedImports: true,
+        alphabetize: {
+          order: 'asc',
+          orderImportKind: 'asc',
+          caseInsensitive: false
+        }
+      }
+    ],
+    'no-restricted-imports': ['error', { patterns: ['../*'] }],
+    'import/no-default-export': ['error'],
+    'import/consistent-type-specifier-style': ['error', 'prefer-top-level'],
+    'import/default': ['off']
   },
   overrides: [
     {
