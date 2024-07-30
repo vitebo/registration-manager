6f7b27b850d7d96951f16f551533c498fba283ab
vitebo <andre.vitebo@quero.com>
2024-07-30 16:01:59 -0300

    chore(context): create store

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
+import { useState } from 'react'
 import Router from "~/router";
 import { Header } from "./components/Header";
+import { StoreContext } from '~/contexts/store-context'
+import { Registration } from '~/entities/registration'
 
 function App() {
+    const [registrations, setRegistrations] = useState<Registration[]>([])
+
   return (
-    <>
+    <StoreContext.Provider value={{ registrations, setRegistrations }}>
       <Header>
         <h1>Caju Front Teste</h1>
       </Header>
       <Router />
-    </>
+    </StoreContext.Provider>
   );
 }
 
---
+++ src/contexts/store-context.ts
@@ -0,0 +1,12 @@
+import { createContext } from "react";
+import { Registration } from '~/entities/registration'
+
+interface StoreContextValue {
+    registrations: Registration[];
+    setRegistrations: (registrations: Registration[]) => void;
+}
+
+export const StoreContext = createContext<StoreContextValue>({
+    registrations: [],
+    setRegistrations: () => {},
+});
