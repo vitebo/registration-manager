76a6dbc2f27c649e3b5fd12de6a0ef395c6362c6
vitebo <andre.vitebo@quero.com>
2024-08-03 15:44:30 -0300

    fix: app build

--- src/docs/Button.stories.tsx
+++ src/docs/Button.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { fn } from '@storybook/test';
-import { Button } from './Button';
+import { Button } from '~/components/Buttons';
 
 const variants = ['primary', 'success', 'danger', 'warning', ''] as const;
 
